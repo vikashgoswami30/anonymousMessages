57c8d4104a878fe454917245f298b12f5a4a5613
vikashgoswami30 <109417405+vikashgoswami30@users.noreply.github.com>
2026-01-22 22:51:13 +0530

    Zod and typescript

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const usernameValidation = z
+  .string()
+  .min(2, "username must be atleast 2 characters")
+  .max(20, "username must be no more than 20 characters")
+  .regex(/^[a-zA-Z0-9_]+$/, "username must not contain special character");
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "password must be atleast 6 characters" }),
+    
+});
